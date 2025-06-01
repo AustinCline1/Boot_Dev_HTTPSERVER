05193c7524680a57710b0e6b6e13ec7bf4b60d4a
Austin Cline <au.cline@hotmail.com>
2025-06-01 17:41:15 -0400

    Added optional parameters for sorting chirps

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
 import {BadRequestError, NotFoundError, UserForbiddenError} from "./errors.js";
-import {createChirp, deleteChirp, getAllChirps, getChirp} from "../db/queries/chirps.js";
+import {createChirp, deleteChirp, getAllChirps, getChirp, getChirpsByAuthor} from "../db/queries/chirps.js";
 import {respondWithJSON} from "./json.js";
 import {getBearerToken, verifyJWT} from "../auth.js";
 import {config} from "../config.js";
@@ -43,6 +43,35 @@ function validateChirp(body: string){
 }
 
 export async function handlerGetAllChirps(req: Request, res: Response) {
+    /* Get chirps using an optional parameter authorId */
+    let authorId = "";
+    let authorIdQuery = req.query.authorId;
+    console.log(`Author ID Query ${authorIdQuery}`);
+    if(typeof authorIdQuery === "string") {
+        authorId = authorIdQuery;
+    }
+    if(authorId) {
+        const chirp = await getChirpsByAuthor(authorId);
+        if(!chirp){
+            throw new NotFoundError("Failed to get chirp");
+        }
+        respondWithJSON(res, 200, chirp);
+        return;
+    }
+    /* Sort chirps by and optional parameter sort=asc or sort=desc */
+    let sort = "asc";
+    let sortQuery = req.query.sort;
+    if(typeof sortQuery === "string") {
+        sort = sortQuery;
+    }
+    if(sort !== "asc" && sort !== "desc") {
+        throw new BadRequestError("Invalid sort parameter");
+    }
+    if(sort === "desc") {
+        const chirps = await getAllChirps("desc");
+        respondWithJSON(res,200, chirps);
+        return
+    }
     const chirps = await getAllChirps();
     if(!chirps) {
         throw new Error("Failed to get chirps");
--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -1,13 +1,17 @@
 import {chirps, NewChirp} from "../schema.js";
 import {db} from "../index.js";
-import {asc, eq} from "drizzle-orm";
+import {asc, desc, eq} from "drizzle-orm";
 
 export async function createChirp(chirp: NewChirp) {
     const [result] = await db.insert(chirps).values(chirp).returning();
     return result;
 }
 
-export async function getAllChirps() {
+export async function getAllChirps(order:string = "asc") {
+    if(order === "desc") {
+        const results = await db.select().from(chirps).orderBy(desc(chirps.createdAt));
+        return results;
+    }
     const results = await db.select().from(chirps).orderBy(asc(chirps.createdAt));
     return results;
 }
@@ -23,4 +27,9 @@ export async function getChirp(id:string) {
 export async function deleteChirp(id:string){
     const result = await db.delete(chirps).where(eq(chirps.id, id));
     return result.length == 0;
+}
+
+export async function getChirpsByAuthor(authorId: string) {
+    const results = await db.select().from(chirps).where(eq(chirps.userId, authorId)).orderBy(asc(chirps.createdAt));
+    return results;
 }
